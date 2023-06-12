655bbd77a30b6a9525fdb9cd61d3f82ddec7df3a
Martin Sejas <martinhugosejas@gmail.com>
2023-06-12 20:18:30 +0200

    Fixed .env configuration issue on linux environment

--- backend/DatabaseConnection.py
+++ backend/DatabaseConnection.py
@@ -4,14 +4,17 @@
 """
 
 import os
+from dotenv import load_dotenv
 import pyodbc
 
 
 class DatabaseConnection:
     def __init__(self) -> None:
-        server = "dsp-group3.database.windows.net"
-        database = "flight-prediction"
-        username = "ais-epita"
+        load_dotenv()
+        server = os.getenv("HOST")
+        database = os.getenv("DATABASE")
+        username = os.getenv("DBUSERNAME")
+        password = os.getenv("PASSWORD")
         password = "DSPgroup3*"
         driver= '{ODBC Driver 18 for SQL Server}'
         self.connection_string = f'DRIVER={driver};SERVER={server};DATABASE={database};UID={username};PWD={password}'
